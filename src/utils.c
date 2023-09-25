e84f73323d373a6328edf800d78f95b170a92c5b
Clang Format <clang-format@karaman.dev>
2023-09-25 07:37:27 +0000

    apply clang-format

--- src/utils.c
+++ src/utils.c
@@ -5,37 +5,40 @@
 
 #define BUFFER_SIZE 256
 
-int getInputInt() {
-  char line[BUFFER_SIZE];
-  int i;
-  if (fgets(line, sizeof(line), stdin)) {
-    if (1 == sscanf(line, "%d", &i)) {
-      return i;
+int getInputInt()
+{
+    char line[BUFFER_SIZE];
+    int i;
+    if (fgets(line, sizeof(line), stdin)) {
+        if (1 == sscanf(line, "%d", &i)) {
+            return i;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
-char getInputChar() {
-  char line[BUFFER_SIZE];
-  char c;
-  if (fgets(line, sizeof(line), stdin)) {
-    if (1 == sscanf(line, "%c", &c)) {
-      return c;
+char getInputChar()
+{
+    char line[BUFFER_SIZE];
+    char c;
+    if (fgets(line, sizeof(line), stdin)) {
+        if (1 == sscanf(line, "%c", &c)) {
+            return c;
+        }
     }
-  }
-  return '\0';
+    return '\0';
 }
 
-char *getInputString(unsigned size) {
-  char *line = (char *)malloc(sizeof(char) * size + 1);
-  if (!line)
+char *getInputString(unsigned size)
+{
+    char *line = (char *)malloc(sizeof(char) * size + 1);
+    if (!line)
+        return NULL;
+
+    if (fgets(line, size, stdin)) {
+        line[strcspn(line, "\n")] = 0;
+        return line;
+    }
+    free(line);
     return NULL;
-
-  if (fgets(line, size, stdin)) {
-    line[strcspn(line, "\n")] = 0;
-    return line;
-  }
-  free(line);
-  return NULL;
 }
